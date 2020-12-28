0a28ae268f7d95ea754a7d7c3eae004402073405
Taideptrai <tai47406@donga.edu.vn>
2020-12-28 09:17:04 +0700

    add old file

---
+++ 2.js
@@ -0,0 +1,11 @@
+// load hết file css rồi mới tới js
+document.addEventListener("DOMContentLoaded",function(){
+    var a = document.getElementsByClassName('top-nav');
+    console.log(a);
+    var b = document.querySelector('form .top-nav-search input[type="text"]')
+    console.log(b);
+    
+    
+   
+    
+},false)
